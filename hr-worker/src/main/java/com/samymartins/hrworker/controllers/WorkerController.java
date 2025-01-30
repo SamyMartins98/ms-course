f9e1a3e460b79667e59f5955d8cf0bb4aa694103
SamyMartins98 <samymartins98@gmail.com>
2025-01-29 21:55:15 -0300

    refresh scope com actuator

--- hr-worker/src/main/java/com/samymartins/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/samymartins/hrworker/controllers/WorkerController.java
@@ -3,6 +3,7 @@ package com.samymartins.hrworker.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.samymartins.hrworker.services.WorkerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @Slf4j
 @RestController
 @RequiredArgsConstructor
