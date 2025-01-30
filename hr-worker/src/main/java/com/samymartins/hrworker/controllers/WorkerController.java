354e1f87f8d70259f68dffa15257de4f7d254c75
SamyMartins98 <samymartins98@gmail.com>
2025-01-29 21:49:50 -0300

    config server client

--- hr-worker/src/main/java/com/samymartins/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/samymartins/hrworker/controllers/WorkerController.java
@@ -2,6 +2,7 @@ package com.samymartins.hrworker.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +13,24 @@ import com.samymartins.hrworker.entities.Worker;
 import com.samymartins.hrworker.services.WorkerService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/workers")
 public class WorkerController {
 	
+	@Value("${test.config}")
+	private String testConfig; 
+	
 	private final WorkerService workerService;
+	
+	@GetMapping("/getConfigs")
+	public ResponseEntity<Void> getConfig() {
+		log.info("CONFIG = "+ testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
